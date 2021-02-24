0e70d4bba184e4552fe08c0c8cd55d88af172cb4
Jon Forsyth <jon4syth@gmail.com>
2021-02-24 09:12:35 -0700

    Setup Petal stack

---
+++ test/petal_web/live/page_live_test.exs
@@ -0,0 +1,11 @@
+defmodule PetalWeb.PageLiveTest do
+  use PetalWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  test "disconnected and connected render", %{conn: conn} do
+    {:ok, page_live, disconnected_html} = live(conn, "/")
+    assert disconnected_html =~ "Boilerplate Generator"
+    assert render(page_live) =~ "Boilerplate Generator"
+  end
+end
